1a249b0e9a53bbf94bf85c87d1b6691d32492592
Arpit Gupta <arpit.gupta2405@gmail.com>
2018-03-07 11:29:40 +0530

    Adding the new route /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todo');
@@ -28,6 +29,24 @@ app.get('/todos', (req, res) => {
   }, (err) => {
     res.status(400).send(err);
   });
+});
+
+//GET todos by ID
+app.get('/todos/:id',(req,res) => {
+  //res.send(req.params);
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)) {
+    res.status(400).end('ID is not valid');
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      res.status(404).send('Todo not found');
+    }
+    // res.send(JSON.stringify(todo, undefined, 2));
+    res.status(200).send({todo});
+  }).catch((e) => res.status(400).send(e));
+
 });
 
 app.listen(3000, () => {
