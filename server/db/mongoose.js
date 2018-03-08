d991d4af7d0d1ed55affac20ba96fd141e37344b
Arpit Gupta <arpit.gupta2405@gmail.com>
2018-03-08 11:53:02 +0530

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,10 @@
 var mongoose = require('mongoose');
 
 var url = 'mongodb://localhost:27017/TodoApp1';
+var mongodb_uri = 'mongodb://arpitgupta2405:qwerty@123@ds261138.mlab.com:61138/todo-app-api'
 
 mongoose.Promise = global.Promise;
-mongoose.connect(url);
+mongoose.connect(url || mongodb_uri);
 
 module.exports = {
   mongoose
