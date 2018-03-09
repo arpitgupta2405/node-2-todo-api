e18450a4843613036802f235a76c64a9dd8cf7c3
Arpit Gupta <arpit.gupta2405@gmail.com>
2018-03-09 09:31:25 +0530

    Modified the server.js

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.post('/todos', (req,res) => {
   });
 
   todo.save().then((doc) => {
-    res.send(doc);
+     return  res.send(doc);
   }, (err) => {
     // res.send(err);
     res.status(400).send(err);
@@ -26,7 +26,7 @@ app.post('/todos', (req,res) => {
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
-    res.send({todos});
+    return res.send({todos});
   }, (err) => {
     res.status(400).send(err);
   });
