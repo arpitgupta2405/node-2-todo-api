897b4998ac5ad8bf6f5a918f6a66f600f5a97a0d
Arpit Gupta <arpit.gupta2405@gmail.com>
2018-03-09 10:55:17 +0530

    Modify mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,7 +5,7 @@ var mongodb_uri = 'mongodb://arpitgupta2405:Gmail123@ds261138.mlab.com:61138/tod
 
 mongoose.Promise = global.Promise;
 // mongoose.connect(mongodb_uri || url);
-mongoose.connect('mongodb://arpitgupta2405:Gmail123@ds261138.mlab.com:61138/todo-app-api' || 'mongodb://localhost:27017/TodoApp1')
+mongoose.connect('mongodb://localhost:27017/TodoApp1' || 'mongodb://arpitgupta2405:Gmail123@ds261138.mlab.com:61138/todo-app-api')
 
 module.exports = {
   mongoose
