6287c747b721eee8e2ccd9b7b1673217c4b78758
Arpit Gupta <arpit.gupta2405@gmail.com>
2018-03-11 01:45:01 +0530

    Some modifications in config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,12 @@
 var env = process.env.NODE_ENV || 'development';
 
-if(env === 'development') {
+if(process.env.PORT){
+  process.env.MONGODB_URI = 'mongodb://arpitgupta2405:Gmail123@ds261138.mlab.com:61138/todo-app-api'
+}else {
   process.env.PORT = 3000;
+if(env === 'development') {
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp1';
 } else if(env === 'test') {
-  process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+}
 }
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose');
 
 
-let db = {
-  localhost: 'mongodb://localhost:27017/TodoApp1',
-  uri: 'mongodb://arpitgupta2405:Gmail123@ds261138.mlab.com:61138/todo-app-api'
-};
+// let db = {
+//   localhost: 'mongodb://localhost:27017/TodoApp1',
+//   uri: 'mongodb://arpitgupta2405:Gmail123@ds261138.mlab.com:61138/todo-app-api'
+// };
 
 mongoose.Promise = global.Promise;
 // mongoose.connect(mongodb_uri || url);
