e01ba8455ca3867c5c9fd838ac8041dc37a80ed7
Arpit Gupta <arpit.gupta2405@gmail.com>
2018-03-15 10:11:11 +0530

    Add POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,15 +1,61 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('User', {
+var UserSchema = mongoose.Schema({
   email: {
     type: String,
     required: true,
     minlength: 1,
-    trim: true
-  }
+    trim: true,
+    unique: true,
+    validate: {
+      validator: (value) => {                            // validator: validator.isEmail === it will work fine also
+        return validator.isEmail(value);
+      },
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
   });
 
-  module.exports ={User};
+UserSchema.methods.toJSON = function () {
+  var user = this;
+  var userObject = user.toObject();
+
+  return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.genAuthToken = function() {
+    var user  = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+    user.tokens = user.tokens.concat([{access, token}]);
+
+    return user.save().then(() => {
+      return token;
+    });
+};
+
+var User = mongoose.model('User', UserSchema);
+
+  module.exports = {User};
 
 // var newUser = new User({
 //   email: 'arpit'
--- server/server.js
+++ server/server.js
@@ -96,6 +96,20 @@ app.patch('/todos/:id', (req, res) => {
   });
 });
 
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    // res.send({user});
+    return user.genAuthToken();
+  }).then((token) => {
+    return res.header('x-auth', token).send(user);
+  }).catch((e) => {
+      res.status(400).send(e);
+  });
+})  ;
+
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
