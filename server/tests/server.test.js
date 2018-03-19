6803c57a56597320aaa2787660759d73e4c27f41
Arpit Gupta <arpit.gupta2405@gmail.com>
2018-03-19 17:41:25 +0530

    Adding the test for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -274,3 +274,55 @@ describe('POST /users', () => {
       .end(done)
 });
 });
+
+describe("POST /users/login", () => {
+
+  it('should login user and return auth token', (done) => {
+    var email = users[1].email;
+    var password = users[1].password;
+
+    request(app)
+      .post('/users/login')
+      .send({email,password})
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toBeTruthy();
+      })
+      .end((err, res) => {
+        if(err){
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.toObject().tokens[0]).toMatchObject({
+             access: 'auth',
+             token: res.headers['x-auth']
+          });
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should reject the invalid login', (done) => {
+    var email = users[1].email;
+    var password = users[1].password+'123456';
+
+    request(app)
+      .post('/users/login')
+      .send({email,password})
+      .expect(400)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toBeFalsy();
+      })
+      .end((err, res) => {
+        if(err){
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+})
