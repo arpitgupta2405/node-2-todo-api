b34c7c96d888f559e637b42bfb5087795dbc85b8
Arpit Gupta <arpit.gupta2405@gmail.com>
2018-03-20 11:05:04 +0530

    Update the config to user json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,19 +1,5 @@
 var env = process.env.NODE_ENV || 'development';
 
-
-
-// if(process.env.PORT){
-//   process.env.MONGODB_URI = 'mongodb://arpitgupta2405:Gmail123@ds261138.mlab.com:61138/todo-app-api'
-// }
-// else {
-//   process.env.PORT = 3000;
-// if(env === 'development') {
-//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp1';
-// } else if(env === 'test') {
-//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-// }
-// }
-
 if (env === 'test' || env === 'development')
 {
     var config = require('./config.json');
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,15 +1,7 @@
 var mongoose = require('mongoose');
 
-
-// let db = {
-//   localhost: 'mongodb://localhost:27017/TodoApp1',
-//   uri: 'mongodb://arpitgupta2405:Gmail123@ds261138.mlab.com:61138/todo-app-api'
-// };
-
 mongoose.Promise = global.Promise;
-// mongoose.connect(mongodb_uri || url);
-// mongoose.connect('mongodb://localhost:27017/TodoApp1' || 'mongodb://arpitgupta2405:Gmail123@ds261138.mlab.com:61138/todo-app-api')
-//process.env.MONGODB_URI = (process.env.PORT ?  db.uri : db.localhost);
+
 mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {
